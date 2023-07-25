c0b399689237da33195644e6a50533492be2000c
Rahul <Rahul@LAPTOP-BMFD2FO4>
2023-07-25 14:03:40 +0530

    update

--- src/src/PositiveNegativeNo.java
+++ src/src/PositiveNegativeNo.java
@@ -1,21 +1,27 @@
 package src;
 
+import java.util.Scanner;
+
 public class PositiveNegativeNo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int i = -2;
+		//int i = -2;
+			Scanner sc = new Scanner(System.in);
+			while(true) {
+			System.out.println("Enter the No : ");
+			int i = sc.nextInt();
 				
 				if(i<0) {
 					
-					System.out.println("The no is Negative");
+					System.out.println("The no " + i + " is Negative");
 				}
 				else {
 					
-					System.out.println("The no is positive");
+					System.out.println("The no "  +  i  +  " is positive");
 				}
-		
+			}
 	}
 
 }
